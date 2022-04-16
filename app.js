3a2b301543044c0186f41e9dd6504a16cdb27bc6
redangelmx <103806083+redangelmx@users.noreply.github.com>
2022-04-16 04:12:50 -0700

    Update app.js

--- app.js
+++ app.js
@@ -117,7 +117,7 @@ function loadVideoURL(url) {
 }
 
 function savePicture() {
-  var dataURL = canvas.toDataURL();
+  var dataURL = canvas.toDataURL("image/jpg");
   var link = document.getElementById("imagelink");
   link.style.display = '';
   link.href = dataURL;
